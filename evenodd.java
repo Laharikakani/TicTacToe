171a925f5972a0084ceec112e535d06077cc0a99
Kakani Lahari <162549268+Laharikakani@users.noreply.github.com>
2024-10-24 21:53:09 +0530

    Update evenodd.java

--- evenodd.java
+++ evenodd.java
@@ -1,14 +1,7 @@
-import java.util.Scanner;
+
 class evenodd{
 	public static void main(String args[]){
-		Scanner a=new Scanner(System.in);
-		int n=a.nextInt();
-		if(n%2==0){
-			System.out.println("even");
-		}
-		else{
-			System.out.println("odd");
-		}
+		System.out.println("Hello java!");
 		
 	}
 }
