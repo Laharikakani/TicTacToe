3b721b3dfe68e57c8f4142e24ee2c69126616638
Kakani Lahari <162549268+Laharikakani@users.noreply.github.com>
2024-10-24 21:22:46 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class evenodd{
+	public static void main(String args[]){
+		Scanner a=new Scanner(System.in);
+		int n=a.nextInt();
+		if(n%2==0){
+			System.out.println("even");
+		}
+		else{
+			System.out.println("odd");
+		}
+		
+	}
+}
